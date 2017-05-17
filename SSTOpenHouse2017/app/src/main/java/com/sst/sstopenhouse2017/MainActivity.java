02c029547daaeac518bae7cd09a8158201518574
joeltio <joeltiojinhon@gmail.com>
2017-05-17 22:03:18 +0800

    Added WebView to MainActivity and template code

--- SSTOpenHouse2017/app/src/main/java/com/sst/sstopenhouse2017/MainActivity.java
+++ SSTOpenHouse2017/app/src/main/java/com/sst/sstopenhouse2017/MainActivity.java
@@ -1,13 +1,14 @@
 package com.sst.sstopenhouse2017;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,35 +19,31 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton redeemFab = (FloatingActionButton) findViewById(R.id.redeem_fab);
+        redeemFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(view.getContext(), RedemptionActivity.class);
+                startActivity(intent);
             }
         });
+
+        WebView mainWebView = (WebView) findViewById(R.id.main_webView);
+        // TODO: Set the webpage to view
+        String webpage = "";
+        mainWebView.loadData(webpage, "text/html; charset=utf-8", "utf-8");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // TODO: fill up menu with items
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
+        // TODO: Setup item on item selected
         return super.onOptionsItemSelected(item);
     }
 }
