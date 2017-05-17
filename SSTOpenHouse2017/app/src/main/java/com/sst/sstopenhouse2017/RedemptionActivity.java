6e43401e5d32b97243fe9ac02feda73125adce86
joeltio <joeltiojinhon@gmail.com>
2017-05-17 21:56:54 +0800

    Added RedemptionActivity

---
+++ SSTOpenHouse2017/app/src/main/java/com/sst/sstopenhouse2017/RedemptionActivity.java
@@ -0,0 +1,13 @@
+package com.sst.sstopenhouse2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RedemptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_redemption);
+    }
+}
