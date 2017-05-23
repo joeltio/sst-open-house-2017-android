2365c70512259430d56ce55135b049549991ca33
joeltio <joeltiojinhon@gmail.com>
2017-05-24 00:05:39 +0800

    Converted RedemptionActivity to RedemptionFragment

--- SSTOpenHouse2017/app/src/main/java/com/sst/sstopenhouse2017/fragments/RedemptionFragment.java
+++ SSTOpenHouse2017/app/src/main/java/com/sst/sstopenhouse2017/fragments/RedemptionFragment.java
@@ -1,24 +1,27 @@
-package com.sst.sstopenhouse2017;
+package com.sst.sstopenhouse2017.fragments;
 
 import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.text.InputFilter;
 import android.text.InputType;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.sst.sstopenhouse2017.R;
 import com.sst.sstopenhouse2017.boothCodes.CodeVerifier;
 
 import java.util.Locale;
 
-public class RedemptionActivity extends AppCompatActivity {
-
+public class RedemptionFragment extends Fragment {
     private boolean redeemCode(String code) {
-        CodeVerifier codeVerifier = new CodeVerifier(code, this);
+        CodeVerifier codeVerifier = new CodeVerifier(code, getActivity());
         switch (codeVerifier.redeem()) {
             case CodeVerifier.CHALLENGE_FAILED_INVALID_CODE_LENGTH:
                 createErrorDialog(getResources().getString(
@@ -39,7 +42,7 @@ public class RedemptionActivity extends AppCompatActivity {
     }
 
     private void createErrorDialog(String message) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(R.string.redemption_dialog_error_header)
                 .setMessage(message);
 
@@ -55,15 +58,15 @@ public class RedemptionActivity extends AppCompatActivity {
     }
 
     private String fetchRedeemedCodesText() {
-        return String.format(Locale.ENGLISH, "%d", CodeVerifier.fetchRedeemedCodes(this));
+        return String.format(Locale.ENGLISH, "%d", CodeVerifier.fetchRedeemedCodes(getActivity()));
     }
 
     private void createRedeemDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(R.string.redemption_dialog_header)
-               .setMessage(R.string.redemption_dialog_description);
+                .setMessage(R.string.redemption_dialog_description);
 
-        final EditText input = new EditText(this);
+        final EditText input = new EditText(getActivity());
         input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS);
         input.setFilters(new InputFilter[] { new InputFilter.LengthFilter(4) });
         input.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
@@ -98,15 +101,20 @@ public class RedemptionActivity extends AppCompatActivity {
 
     private TextView redeemedCodes;
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_redemption);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_redemption, container, false);
+    }
 
-        this.redeemedCodes = (TextView) findViewById(R.id.redemption_count);
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        this.redeemedCodes = (TextView) getActivity().findViewById(R.id.redemption_count);
         redeemedCodes.setText(fetchRedeemedCodesText());
 
-        Button redeemButton = (Button) findViewById(R.id.redemption_button);
+        Button redeemButton = (Button) getActivity().findViewById(R.id.redemption_button);
         redeemButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
